7ffed12c2c9ed537c6eb6ea0da6b3f9166296981
dizilayz <maksimsinkevich00@yandex.ru>
2022-10-01 23:10:20 +0300

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// /*Эльвира: 7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8*/
+
+
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if ((number > 99 && number < 1000) || (number < -99 && number > -1000))
+{
+
+
+    int lastnumber = number % 10;
+    if (lastnumber < 0)
+    {
+        lastnumber = lastnumber * -1;
+    }
+    Console.WriteLine(lastnumber);
+
+}
+else
+{
+    Console.WriteLine("Введено некорректное значение");
+
+}
+
